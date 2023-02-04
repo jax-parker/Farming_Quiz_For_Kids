26d897cf4afb691536b7f496ec4061cf8eb30416
jax-parker <parkyjax711@gmail.com>
2023-02-04 12:28:36 +0000

    lay out function structure for game

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+
+/**
+ * Collect name entered in input box by user
+ */
+function nameFunction() {
+
+}
+
+/**
+ * Function to start the game
+ */
+function StartGame() {
+
+}
+
+/**
+ * Function to set the next question
+ */
+function showQuestion() {
+
+}
+
+/**
+ * Function to show if answer was correct or wrong */
+function setStatusClass() {
+
+}
+/**
+ * Function to clear the status class
+ */
+function clearStatusClass() {
+
+}
+
+
+/**
+ * Function to clear answers and set the next question
+ */
+function resetState() {
+
+}
+
+/**
+ * Function to select the correct answer & set loop limit
+ */
+
+function selectAnswer() {
+
+}
+
+/*List of questions for quiz*/
+
+
