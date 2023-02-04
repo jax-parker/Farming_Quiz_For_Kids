76511bae946ff3a89fecbf2aab4629a67d6d23fa
jax-parker <parkyjax711@gmail.com>
2023-02-04 12:40:03 +0000

    add nameFunction and check event listener on start click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
-
+/* grab elements from html*/
+const startButton = document.getElementById('start-btn');
+const nextButton= document.getElementById('next-btn');
+const scoreContainer = document.querySelector('.score-container');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+let shuffledQuestions, currentQuestionsIndex;
 
 /**
  * Collect name entered in input box by user
  */
 function nameFunction() {
-
+    let userInput = document.querySelector('#userInput');
+    let message = document.querySelector('#message');
 }
+//when start button is clicked - execute startGame
 
+startButton.addEventListener('click', startGame);
+console.log('started');
 /**
  * Function to start the game
  */
-function StartGame() {
+function startGame() {
 
 }
 
